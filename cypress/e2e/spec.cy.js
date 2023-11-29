cbb257dcd790a9d2ab6aa9e7217c8d114a0dbe1a
Aly <alyolive94@gmail.com>
2023-11-29 16:26:59 -0300

    Teste tsmoo

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,30 +1,41 @@
+// const { type } = require("cypress/types/jquery");
+
 describe('Meu Primeiro Teste', () => {
   it('Deve verificar o título da página', () => {
     // Visita a URL da página que você quer testar
-    cy.visit('https://portal.logsyst.com.br');
+    cy.visit('https://portal-hml.tismoo.me');
 
     //Realizar login
-    cy.get('.sc-dSCufp > #email').click().type("coorp100@saopaulo.com.br")
-    cy.get('.sc-dSCufp > #password').click().type("@devsViptech1")
-    cy.get('.sc-fPXMVe').click()
+    cy.get('.sc-bypJrT > #email').click().type("aabatista@viptech.com.br")
+    cy.get('.sc-bypJrT > #password').click().type("1234@5678a")
+    cy.get('.fTNabx').click()
 
-    //Acessar opção "Cadastros"
-    cy.get(':nth-child(4) > .sc-gweoQa').click()
-
-    //Validar se é a aba de áreas
-    cy.get('.dVtNse').contains("Áreas")
-
-    //Acessar rastreadores 
-    cy.get('[href="/registers/trackers"]').click()
-
-    //Validar se é a aba de Rastreadores
-    cy.get('.dVtNse').contains('Rastreadores')
-
-    //Acessar a opçao de cadastrar rastreador
-    cy.get('.khtPsb > .sc-fPXMVe').click()
+    //Acessar opção "Perfil de saúde" 
+    cy.get('[href="/patient-profiles"] > .sc-kgOKUu').click()
 
     //Validade se apareceu o modal 
-    cy.contains('Cadastrar Rastreador')
+    cy.contains('Adicionar Perfil de Saúde')
+
+    //Adicionar perfil de saúde
+    cy.get('.sc-cXPBUD > .sc-eDPEul').click()
+
+    //Inserir o primeiro nome 
+    cy.get('input#name.sc-dSCufp.egyFrY').click().type("TEST QA")
+
+    //Inserir o segundo nome
+    cy.get('input#lastName.sc-dSCufp.egyFrY').click().type("QQA")
+
+    //Sexo 
+    
+    cy.get('li\x30.sc-hZDyAQ hEWFcA')
+
+      // cy.get('input#sc-ibQAlb.cTitmG').click()
+      // // Use o método invoke para definir o valor do atributo placeholder
+      // .invoke('placeholder');
+
+    
+
+
 
   });
 });
